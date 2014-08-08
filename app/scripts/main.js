bb033979afe0c21d6123310306d12a52953b4149
Drew Botka <hellodrewbot@gmail.com>
2014-08-08 19:31:36 -0400

    added router resources, routes, model, gallery controller and app and gallery templates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,3 +15,107 @@ App.ApplicationAdapter = DS.FirebaseAdapter.extend({
   });
 
 App.ApplicationSerializer = DS.FirebaseSerializer.extend();
+
+///////////////////////////////////////////////////////////////////////
+//////////////////////// Routers //////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+App.Router.map(function(){
+	this.resource('app', function(){
+		this.resource('gallery')
+		this.resource('about');
+		this.resource('blog');
+		this.resource('contact');
+		this.resource('session', {path: ':session_id/session'});
+	});
+
+});
+
+///////////////////////////////////////////////////////////////////////
+////////////////////////// Routes /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+// Index redirect
+App.IndexRoute = Ember.Route.extend({
+	redirect: function() {
+	    this.transitionTo('gallery');
+    }
+});
+
+App.AppRoute = Ember.Route.extend({
+    model: function(params) {
+      return this.store.findAll("portfolio");
+    }
+});
+
+App.GalleryRoute = Ember.Route.extend({
+    model: function(params) {
+      return this.store.findAll("portfolio");
+    }
+});
+
+App.SessionRoute = Ember.Route.extend({
+    model: function(params) {
+      return this.store.find("portfolio", params.session_id);
+    }
+});
+
+///////////////////////////////////////////////////////////////////////
+////////////////////////// Models /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+
+// portfolio
+var attr = DS.attr;
+
+App.Portfolio = DS.Model.extend({
+	title: attr('string'),
+	date: attr('string'),
+    thumbnail: attr('file'),
+	introCopy: attr('string'),
+	imageOne: attr('file'),
+	blogCopyOne: attr('string'),
+	imageTwo: attr('file'),
+	blogCopyTwo: attr('string'),
+	imageThree: attr('file'),
+	blogCopyThree: attr('string'),
+	imageFour: attr('file'),
+	blogCopyFour: attr('string'),
+	imageFive: attr('file'),
+	blogCopyFive: attr('string'),
+	imageSix: attr('file'),
+	blogCopySix: attr('string'),
+	imageSeven: attr('file'),
+	blogCopySeven: attr('string'),
+	imageEight: attr('file'),
+	blogCopyEight: attr('string'),
+	imageNine: attr('file'),
+	blogCopyNine: attr('string'),
+	imageTen: attr('file'),
+	blogCopyTen: attr('string')
+});
+
+///////////////////////////////////////////////////////////////////////
+//////////////////// Controllers //////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+// All Text Fields
+Ember.TextField.reopen({
+    classNames: ['all-text-inputs']
+});
+
+//////////////////////////// Dashboard Controller /////////////////////////////////
+App.GalleryController = Ember.ArrayController.extend({
+	itemController: 'portfolio',
+
+	actions:{
+
+	}
+
+});
+
+
+
+
+
+
