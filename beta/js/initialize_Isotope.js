343415390ce4edf1e3164103e1cf56b0b5558643
Drew Botka <hellodrewbot@gmail.com>
2014-09-05 16:58:20 -0400

    added new logo and mark, updated gallery responsiveness, changed p font, added filters

--- beta/js/initialize_Isotope.js
+++ beta/js/initialize_Isotope.js
@@ -4,13 +4,16 @@ $('.container').isotope({
   itemSelector: '.item',
   layoutMode: 'masonry',
   columnWidth: 300,
-  gutter: 10
 });
 
 
 
 $('.filter-one').click(function(){
 	$('.container').isotope({ filter: '.item-one' })
+})
+
+$('.filter-two').click(function(){
+	$('.container').isotope({ filter: '.item-two' })
 })
 
 $('.filter-all').click(function(){
