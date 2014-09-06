0f2bb31a64cc0b55e7716b3179cc17670e9e87c3
Drew Botka <hellodrewbot@gmail.com>
2014-09-06 15:50:43 -0400

    cleaned up header, made more semantic, removed lightbox and menu js, centered logo

--- beta/js/initialize_Isotope.js
+++ beta/js/initialize_Isotope.js
@@ -1,13 +1,19 @@
+// Isotope controls
+// Docs: http://isotope.metafizzy.co/
 
-
+// Initialize Isotope and set grid container by calling class
 $('.container').isotope({
+  // Set items to be placed in grid by calling class
   itemSelector: '.item',
+  // Set layout mode to masonry ("pinterest style"): see docs http://isotope.metafizzy.co/layout-modes.html
   layoutMode: 'masonry',
+  // Set column width
   columnWidth: 300,
 });
 
-
-
+// Filters
+// Call button by class for click action to be applied to
+// Filter within a specified container by specified classname
 $('.filter-one').click(function(){
 	$('.container').isotope({ filter: '.item-one' })
 })
