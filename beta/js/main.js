607de7e13d4c3a9ce108274119d0e1549f914762
Drew Botka <hellodrewbot@gmail.com>
2014-09-07 10:25:58 -0400

    added mouseover and mouseout events to gallery items showing their alt tag on hover

--- beta/js/main.js
+++ beta/js/main.js
@@ -1,7 +1,33 @@
 
+// Click to show and hide nav
 $('.menu').click(function(){
 	$('.nav').toggleClass('show-nav');
 	$('.menu').toggleClass('menu-open');
 })
 
+// *--- Click outside of nav to hide when nav is visible --*
+// $(document).on('click', function(event) {
+//   if (!$(event.target).closest('#menucontainer').length) {
+//     // Hide the menus.
+//   }
+// });
 
+$(document).click(function(event){
+	if (!$(event.target).closest('.nav').length) {
+
+	}
+})
+
+// Mouseover events
+var $overlay = $('<div id="overlay"></div>');
+var $caption = $("<p></p>");
+
+$( ".item" )
+  .mouseover(function() {
+    var captionText = $(this).children("img").attr("alt");
+    console.log(captionText)
+  })
+
+  .mouseout(function() {
+    console.log('mouseout')
+  });
