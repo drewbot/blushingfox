30cf04b0637cd03b561ee3b86caeae8a3314e3ff
Drew Botka <hellodrewbot@gmail.com>
2014-09-09 09:52:13 -0400

    updated sub pages to contain global header

--- beta/js/main.js
+++ beta/js/main.js
@@ -6,28 +6,26 @@ $('.menu').click(function(){
 })
 
 // *--- Click outside of nav to hide when nav is visible --*
-// $(document).on('click', function(event) {
-//   if (!$(event.target).closest('#menucontainer').length) {
-//     // Hide the menus.
-//   }
-// });
-
-$(document).click(function(event){
-	if (!$(event.target).closest('.nav').length) {
-
+$(document).on('click', function() {
+  	if (!$(this).hasClass('nav')) {
+	  	if ($('.nav').hasClass('show-nav')) {
+			console.log('true that')
+  		} else {
+  			console.log('woop woop')
+  		}
 	}
-})
+});
 
 // Mouseover events
-var $caption = $('<p class="overlay-caption"></p>');
 var $overlay = $('<div class="item-overlay"></div>');
+var $caption = $('<p class="overlay-caption"></p>');
 var $icon = $('<p class="overlay-icon">z</p>');
 
 $('.item').mouseover(function() {
     var captionText = $(this).find('img').attr('alt');
     $caption.text(captionText);
     var overlayHeight = $(this).find('img').height(); // This fixes the problem of the overlay being the same height as the container which was a few pixels taller than the image because some weird anchor tag thing
-    $overlay.height(overlayHeight)
+    $overlay.height(overlayHeight);
     $(this).children('a').append($overlay);
     $(this).children('a').append($caption);
     $(this).children('a').append($icon);
