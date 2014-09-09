0c4e25bbe9995414ce85232337e45095305fd977
Drew Botka <hellodrewbot@gmail.com>
2014-09-09 00:07:11 -0400

    set overlay height to match image height

--- beta/js/initialize_Isotope.js
+++ beta/js/initialize_Isotope.js
@@ -3,12 +3,15 @@
 
 // Initialize Isotope and set grid container by calling class
 $('.container').isotope({
-  // Set items to be placed in grid by calling class
-  itemSelector: '.item',
-  // Set layout mode to masonry ("pinterest style"): see docs http://isotope.metafizzy.co/layout-modes.html
-  layoutMode: 'masonry',
-  // Set column width
-  columnWidth: 300,
+	// Set items to be placed in grid by calling class
+	itemSelector: '.item',
+	// Set layout mode to masonry ("pinterest style"): see docs http://isotope.metafizzy.co/layout-modes.html
+	layoutMode: 'masonry',
+	// Set column width
+	masonry: {  // Keeping this as reference to consider percentage based image width
+	  	columnWidth: '',
+	  	gutter: ''
+	}
 });
 
 // Filters
--- beta/js/main.js
+++ beta/js/main.js
@@ -20,13 +20,16 @@ $(document).click(function(event){
 
 // Mouseover events
 var $caption = $('<p class="overlay-caption"></p>');
-var $overlay = $('<div class="item-overlay">')
+var $overlay = $('<div class="item-overlay"></div>');
+var $icon = $('<p class="overlay-icon">z</p>');
 
 $('.item').mouseover(function() {
     var captionText = $(this).find('img').attr('alt');
-    console.log(captionText)
     $caption.text(captionText);
-    $(this).append($overlay);
-    $(this).append($caption);
+    var overlayHeight = $(this).find('img').height(); // This fixes the problem of the overlay being the same height as the container which was a few pixels taller than the image because some weird anchor tag thing
+    $overlay.height(overlayHeight)
+    $(this).children('a').append($overlay);
+    $(this).children('a').append($caption);
+    $(this).children('a').append($icon);
 })
 
