a826a32d483a2bb1b4ff3b95c37542d6436baeae
Drew Botka <hellodrewbot@gmail.com>
2014-10-05 18:36:53 -0400

    added contact form and styled about page

--- beta/js/initialize_Isotope.js
+++ beta/js/initialize_Isotope.js
@@ -1,17 +1,19 @@
 // Isotope controls
 // Docs: http://isotope.metafizzy.co/
 
-// Initialize Isotope and set grid container by calling class
-$('.dynamic-gallery').isotope({
-	// Set items to be placed in grid by calling class
-	itemSelector: '.item',
-	// Set layout mode to masonry ("pinterest style"): see docs http://isotope.metafizzy.co/layout-modes.html
-	layoutMode: 'masonry',
-	// Set column width
-	masonry: {  // Keeping this as reference to consider percentage based image width
-	  	columnWidth: '',
-	  	gutter: ''
-	}
+$(document).ready(function() {
+	// Initialize Isotope and set grid container by calling class
+	$('.dynamic-gallery').isotope({
+		// Set items to be placed in grid by calling class
+		itemSelector: '.item',
+		// Set layout mode to masonry ("pinterest style"): see docs http://isotope.metafizzy.co/layout-modes.html
+		layoutMode: 'masonry',
+		// Set column width
+		// masonry: {  // Keeping this as reference to consider percentage based image width
+		//   	columnWidth: 300,
+		//   	gutter: 10
+		// }
+	});
 });
 
 // Filters
